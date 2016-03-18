36cfa128ea840165ac09311eb60ad2993a93dfdb
fchabot <f.chabot@gmail.com>
2016-03-17 21:17:24 -0400

    project init commit

---
+++ Unity/UnityNetworking/Assets/ApplicationLauncher.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationLauncher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("Hey!");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
